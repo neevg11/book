3d8c8d3430cdd44bb86e67f4374b7591659142f7
neevg11 <neevg@mail.ru>
2020-06-11 17:27:09 +0300

    Update book.py

--- book.py
+++ book.py
@@ -1,4 +1,19 @@
 import os
 
 
+path = 'd:\\books'
 
+def dir_file(path, level=1):
+    for i in os.listdir(path):
+        if os.path.isfile(path+'\\'+i):
+            x = path+'\\'+i
+            print(x.split('\\'))
+            #print(x.split('\\')[3])
+            print('Путь у файлу: ' + x)
+
+    for i in os.listdir(path):
+        if os.path.isdir(path+'\\'+i):
+            dir_file(path+'\\'+i)
+
+
+dir_file(path)
